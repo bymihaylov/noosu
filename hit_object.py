d4808bc986e6ca98a1a1d62fff1162e0f19e16c5
bymihaylov <boyan.mihailov50@gmail.com>
2023-12-24 10:16:01 +0200

    Using tuple for position simplifies work with pygame

--- hit_object.py
+++ hit_object.py
@@ -3,8 +3,7 @@ from typing import Any
 
 class HitObject:
     def __init__(self, x: int, y: int, time: int, type: enum.IntFlag, hitSound: enum.IntFlag, objectParams: list[Any], hitSample: str) -> None:
-        self._x = x
-        self._y = y
+        self.__pos: tuple[int, int] = x, y
         self._time = time
         self._type = type
         self._hitSound = hitSound
@@ -13,11 +12,15 @@ class HitObject:
 
     @property
     def x(self) -> int:
-        return self._x
+        return self.__pos[0]
 
     @property
     def y(self) -> int:
-        return self._y
+        return self.__pos[1]
+    
+    @property
+    def xy_position(self) -> tuple(int, int):
+        return self.__pos
 
     @property
     def time(self) -> int:
