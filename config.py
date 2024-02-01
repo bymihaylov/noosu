251d26e01e09f29e0e4153fdf5b38a3e3eda03ef
bymihaylov <boyan.mihailov50@gmail.com>
2024-02-01 18:57:10 +0200

    Add useful comments

--- config.py
+++ config.py
@@ -4,8 +4,19 @@ assets_dir = Path("assets")
 external_packs_dir = Path("external_packs")
 font_dir = Path("fonts")
 
-width = 640
-height = 480
+## 640x480
+# width = 640
+# height = 480
+## 1920x1080
+width = 1920
+height = 1080
+## 1366x768
+# width = 1366
+# height = 768
+## 1536x864
+# width = 1536
+# height = 868
+
 fps = 60
 
 # Colours
--- playfield.py
+++ playfield.py
@@ -8,6 +8,16 @@ from noosu_object import NoosuObject
 from hit_circle import HitCircle
 import pygame
 
+"""
+In osu!, circle size changes the size of hit circles and sliders, with higher values creating smaller hit objects. 
+Spinners are unaffected by circle size. Circle size is derived through the following formula:
+
+r = 54.4 - 4.48 * CS
+
+Where r is the radius measured in osu!pixels, and CS is the circle size value.
+-------
+source: https://osu.ppy.sh/wiki/en/Beatmap/Circle_size
+"""
 
 class Playfield(Scene):
     def __init__(self, dot_osu_path: str):
@@ -17,6 +27,7 @@ class Playfield(Scene):
         self.hit_obj_index = 0
 
     def setup(self):
+        # pygame.mixer.Sound
         pass
 
     def handle_events(self, events):
@@ -24,8 +35,37 @@ class Playfield(Scene):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 print("Click")
 
+
+    def gamefield_to_screenspace(self, gamefield: tuple[int, int]) -> tuple[int, int]:
+        """
+        A function that converts gamefield coordinates to screen space.
+        Args:
+            gamefield: tuple[int, int]
+
+        Returns:
+            screenspace: tuple[int, int]
+        """
+        pass
+
+    def screenspace_to_gamefield(self, screenspace: tuple[int, int]) -> tuple[int, int]:
+        """
+        A function that converts screen space coordinates to gamefield.
+        Args:
+            screenspace: tuple[int, int]
+
+        Returns:
+            gamefield: tuple[int, int]
+        """
+        pass
+
     def update(self, dt):
-        hit_circle = HitCircle(config.blue, self.noosu.hit_objects[self.hit_obj_index].xy_position, 1)
+        relative_coords: tuple[int, int] = self.noosu.hit_objects[self.hit_obj_index].xy_position
+        # pos: tuple[int, int] = (relative_coords[0] + (config.width - 512) // 2, relative_coords[1] + (config.height - 364) // 2)
+
+        x, y = relative_coords
+        pos: tuple[int, int] = (config.width * 0.2 + x * 1.8, config.height * 0.12 + y * 1.8) # magic?
+        print(f"{relative_coords=}\t{pos=}")
+        hit_circle = HitCircle(config.blue, relative_coords, 1)
         # print(f"{len(self.noosu.hit_objects)=}")
         if self.hit_obj_index + 1 < len(self.noosu.hit_objects):
             if self.noosu.hit_objects[self.hit_obj_index].type & bit_flags.HitObjectType.HIT_CIRCLE:
