d9ec10c63369f7ff35e5d1cb1f2931f5ec9a55ed
bymihaylov <boyan.mihailov50@gmail.com>
2024-02-09 22:55:53 +0200

    clean main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
 from config import config
-from src.scene.playfield import Playfield
 from src.scene.setup_scene import Setup
 import pygame
 
 if __name__ == "__main__":
+
     pygame.init()
     pygame.mixer.init()
     pygame.display.set_caption("noosu!")
@@ -14,10 +14,7 @@ if __name__ == "__main__":
     clock = pygame.time.Clock()
     running = True
 
-    scene = Playfield(Path("assets/355322 nekodex - circles!/nekodex - circles! (pishifat) [insane!].osu"))
-    # scene = Setup()
-
-    scene.setup()
+    scene = Setup()
 
     ## Game loop
     while running:
@@ -27,6 +24,8 @@ if __name__ == "__main__":
         scene.handle_events(events)
         scene.update(dt)
         scene.render(screen)
+
+        scene = scene.next
 
         for event in events:
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
