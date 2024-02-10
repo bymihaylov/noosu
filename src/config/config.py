47109d4b25754d31854106aa2928efe75443bbcb
bymihaylov <boyan.mihailov50@gmail.com>
2024-02-10 14:01:37 +0200

    ui

--- src/config/config.py
+++ src/config/config.py
@@ -1,8 +1,12 @@
+from enum import Enum
 from pathlib import Path
+
+import pygame.color
 
 assets_dir = Path("assets")
 external_packs_dir = Path("external_packs")
 font_dir = Path("fonts")
+ui_dir = Path("ui")
 
 song_select_menu_image_resolution: tuple[int, int] = 1280, 720
 
@@ -27,3 +31,10 @@ red = 250, 0, 0
 green = 0, 250, 0
 blue = 0, 0, 250
 white = 250, 250, 250
+
+
+class Colour(pygame.color.Color):
+    backround = 53, 47, 68
+    purple = 92, 84, 112
+    light_purple = 185, 180, 199
+    foreground = 250, 240, 230
