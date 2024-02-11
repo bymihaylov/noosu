b67ee1f0279900a8c1e44c9951e4397a88f0ae53
bymihaylov <boyan.mihailov50@gmail.com>
2024-02-11 16:58:15 +0200

    get .stem of Path object, not of a string

--- src/noosu/parse_beatmap.py
+++ src/noosu/parse_beatmap.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 def uncompress_archive(src_path: str) -> None:
     with zipfile.ZipFile(src_path, "r") as zip_ref:
-        path = Path(config.assets_dir, src_path.stem)
+        path = config.assets_dir / Path(src_path).stem
         zip_ref.extractall(path=path)
 
 
